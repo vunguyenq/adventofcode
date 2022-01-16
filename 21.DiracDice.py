2926337ede3d83fa3bf8374b69d7c80500428866
vunguyenq <vu.nguyenq89@gmail.com>
2022-01-16 16:34:03 +0100

    drafted part2

--- 21.DiracDice.py
+++ 21.DiracDice.py
@@ -1,7 +1,9 @@
 import datetime
+from itertools import product
+from collections import Counter
 
-exec_part = 1 # which part to execute
-exec_test_case = 0 # -1 = all test inputs, n = n_th test input; 0 = real puzzle input
+exec_part = 2 # which part to execute
+exec_test_case = -1 # -1 = all test inputs, n = n_th test input; 0 = real puzzle input
 
 # Puzzle input
 with open('input/input_test21.txt') as f:
@@ -28,9 +30,53 @@ def part1(input):
         i += 1
     return i * 3 * min(p1_score, p2_score)
 
+# Unique values of 3 dice rolls + frequency {6: 7, 5: 6, 7: 6, 4: 3, 8: 3, 3: 1, 9: 1}
+DICE_VALUES = Counter([sum(c) for c in list(product([1,2,3], repeat = 3))])
+STATE_CACHE = {}
+
+def recursive_count(p1_pos, p1_score, p2_pos, p2_score, turn):
+    # Stop recursion when a player wins
+    if p1_score >= 21:
+        return [1, 0]
+    if p2_score >= 21:
+        return [0, 1]
+
+    # Return if a state was seen before
+    state_key = (p1_pos, p1_score, p2_pos, p2_score, turn)
+    if state_key in STATE_CACHE:
+        return STATE_CACHE[state_key]
+
+    # Current player roll dices
+    universe_count = [0, 0]
+    if turn == 1:
+        for dice_val in DICE_VALUES:
+            new_pos = (p1_pos + dice_val - 1) %10 + 1
+            new_score = p1_score + new_pos 
+            #print(p1_pos, p1_score, new_pos, new_score, dice_val, DICE_VALUES[dice_val])
+            universe_count[0] += DICE_VALUES[dice_val] * recursive_count(new_pos, new_score, p2_pos, p2_score, 2)[0]
+    else:
+        for dice_val in DICE_VALUES:
+            new_pos = (p2_pos + dice_val - 1) %10 + 1
+            new_score = p2_score + new_pos 
+            universe_count[1] += DICE_VALUES[dice_val] * recursive_count(p1_pos, p1_score, new_pos, new_score, 1)[1]
+    
+    STATE_CACHE[state_key] = universe_count
+    return universe_count
+    # 7 unique dice combination * freq of each unique
+    # Cache 
+    # https://www.reddit.com/r/adventofcode/comments/rm7ygy/2021_day_21_part2_could_someone_explain_the/
+    
+
+
 def part2(input):
-    result = 0
-    return result
+    p1_pos, p2_pos = input
+    a = recursive_count(p1_pos, 0, p2_pos, 0, 1)
+    print(a)
+    state_keys = list(STATE_CACHE.keys())
+    print(len(state_keys))
+    for k in state_keys[-100:]:
+        print(k, STATE_CACHE[k])
+    return 0
 
 if __name__ == "__main__":
     if(exec_test_case == 0):
