6ba07a61e87370136216a354745596ca3edbd00d
vunguyenq <vu.nguyenq89@gmail.com>
2022-12-08 16:27:28 +0100

    day 06 part 2 done

---
+++ 2022/06.TuningTrouble.py
@@ -0,0 +1,60 @@
+import datetime
+import os
+
+exec_part = 2 # which part to execute
+exec_test_case = 0 # -1 = all test inputs, n = n_th test input; 0 = real puzzle input
+
+# Puzzle input
+dirname = os.path.dirname(__file__)
+with open(os.path.join(dirname, 'input/input_test06.txt')) as f:
+    INPUT_TEST = f.read()
+
+with open(os.path.join(dirname, 'input/input06.txt')) as f:
+    INPUT = f.read()   
+
+def parse_input(input):
+    return input
+
+def part1(input):
+    for i in range(4, len(input)):
+        if(len(set(input[i-4: i])) == 4):
+            return i
+    return None
+
+def part2(input):
+    for i in range(14, len(input)):
+        if(len(set(input[i-14: i])) == 14):
+            return i
+    return 0
+
+if __name__ == "__main__":
+    if(exec_test_case == 0):
+        inputs = [INPUT]
+    else:
+        inputs = INPUT_TEST.split("\n#####INPUT_SEPERATOR#####\n")
+    
+    if exec_test_case > len(inputs):
+        print(f"Test case {exec_test_case} does not exist")
+        quit()
+    for i, input_str in enumerate(inputs):
+        if(exec_test_case == 0):
+            print(f"Running real puzzle input...")
+        elif (exec_test_case == -1):
+            print(f"Running test case {i+1}/{len(inputs)}...")
+        else:
+            if (i+1 == exec_test_case):
+                print(f"Running test case {i+1}/{len(inputs)}...")
+            else:
+                continue
+            
+        input = parse_input(input_str)
+        start_time = datetime.datetime.now() 
+        if (exec_part == 1):
+            result = part1(input)
+        else:
+            result = part2(input)
+        end_time = datetime.datetime.now() 
+
+        print('Part {} time: {}'.format(exec_part, end_time - start_time))
+        print('Part {} answer: {}\n'.format(exec_part, result))
+
