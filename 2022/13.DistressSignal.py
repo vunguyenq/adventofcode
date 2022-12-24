ba9a1b6867927a13032c36bd9cfc9e320d554b7f
vunguyenq <vu.nguyenq89@gmail.com>
2022-12-25 00:00:21 +0100

    day 13 part 1 done

---
+++ 2022/13.DistressSignal.py
@@ -0,0 +1,85 @@
+import datetime
+import os
+
+exec_part = 1 # which part to execute
+exec_test_case = 0 # -1 = all test inputs, n = n_th test input; 0 = real puzzle input
+
+# Puzzle input
+dirname = os.path.dirname(__file__)
+with open(os.path.join(dirname, 'input/input_test13.txt')) as f:
+    INPUT_TEST = f.read()
+
+with open(os.path.join(dirname, 'input/input13.txt')) as f:
+    INPUT = f.read()   
+
+def parse_input(input):
+    return [[eval(l) for l in p.split('\n')] for p in input.split('\n\n')]
+
+def compare(left, right):
+    """
+        Recursively compare 2 values left and right
+        Return True if values come in the right order, False if not, None if cannot decide.
+    """
+    if type(left) is int and type(right) is int:
+        if left < right:
+            return True
+        elif left > right:
+            return False
+        else:
+            return None
+    if type(left) is int and type(right) is list:
+        return compare([left], right)
+    if type(left) is list and type(right) is int:
+        return compare(left, [right])
+    # left and right are both lists
+    for i in range(len(left)):
+        if i == len(right): # right runs out of items first
+            return False
+        compare_element = compare(left[i], right[i])
+        if compare_element is not None:
+            return compare_element
+    if len(left) < len(right): # left runs out of items first
+        return True
+    return None
+
+def part1(input):
+    right_order_pairs =[]
+    for i, pair in enumerate(input):
+        if(compare(*pair)):
+            right_order_pairs.append(i+1)
+    return sum(right_order_pairs)
+
+def part2(input):
+    return 0
+
+if __name__ == "__main__":
+    if(exec_test_case == 0):
+        inputs = [INPUT]
+    else:
+        inputs = INPUT_TEST.split("\n#####INPUT_SEPERATOR#####\n")
+    
+    if exec_test_case > len(inputs):
+        print(f"Test case {exec_test_case} does not exist")
+        quit()
+    for i, input_str in enumerate(inputs):
+        if(exec_test_case == 0):
+            print(f"Running real puzzle input...")
+        elif (exec_test_case == -1):
+            print(f"Running test case {i+1}/{len(inputs)}...")
+        else:
+            if (i+1 == exec_test_case):
+                print(f"Running test case {i+1}/{len(inputs)}...")
+            else:
+                continue
+            
+        input = parse_input(input_str)
+        start_time = datetime.datetime.now() 
+        if (exec_part == 1):
+            result = part1(input)
+        else:
+            result = part2(input)
+        end_time = datetime.datetime.now() 
+
+        print('Part {} time: {}'.format(exec_part, end_time - start_time))
+        print('Part {} answer: {}\n'.format(exec_part, result))
+
