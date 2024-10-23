93adefa33fcca910219a26a9fc802133dbd7abcc
vunguyenq <vu.nguyenq89@gmail.com>
2024-10-23 23:27:37 +0200

    2023 day 15 part 2

--- 2023/15.LensLibrary.py
+++ 2023/15.LensLibrary.py
@@ -1,7 +1,7 @@
 import datetime
 import os
 
-exec_part = 1  # which part to execute
+exec_part = 2  # which part to execute
 exec_test_case = 0  # -1 = all test inputs, n = n_th test input; 0 = real puzzle input
 
 # Puzzle input
@@ -21,11 +21,35 @@ def compute_hash(txt):
         current_value = (current_value + ord(c)) * 17 % 256
     return current_value
 
+class Box:
+    def __init__(self, box_id: int):
+        self.box_id = box_id
+        self.lenses = {}
+
+    def add_or_replace_lens(self, lens_label: str, lens_focal_length: int):
+        self.lenses[lens_label] = lens_focal_length
+
+    def remove_lens(self, lens_label: str):
+        _ = self.lenses.pop(lens_label, None)
+
+    def compute_lenses_power(self):
+        return sum([(self.box_id + 1) * (i + 1) * f for i, f in enumerate(self.lenses.values())])
+
 def part1(input):
     return sum([compute_hash(s) for s in input])
 
 def part2(input):
-    return 0
+    boxes = [Box(i) for i in range(256)]
+    for seq in input:
+        if '-' in seq:
+            lens_label = seq[:-1]
+            boxes[compute_hash(lens_label)].remove_lens(lens_label)
+        elif '=' in seq:
+            lens_label, lens_focal_length = seq.split('=')
+            boxes[compute_hash(lens_label)].add_or_replace_lens(lens_label, int(lens_focal_length))
+        else:
+            continue
+    return sum([box.compute_lenses_power() for box in boxes])
 
 
 if __name__ == "__main__":
